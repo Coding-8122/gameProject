e7fbe425eb7854f6727f3adea5ba7cda79d171d6
Coding-8122 <hyderahussain@gmail.com>
2026-03-02 13:49:53 +0000

    chess board complete - wroking on header logic

---
+++ build/CMakeFiles/lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1.dir/main.cpp.obj"
+  "CMakeFiles/lab_1.dir/main.cpp.obj.d"
+  "bin/lab_1.exe"
+  "bin/lab_1.exe.manifest"
+  "bin/lab_1.pdb"
+  "liblab_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,20 +1,43 @@
 #include <SFML/Graphics.hpp>
-
+#include "piece.h"
+#include "MechanicsHeader.h"
+#include "button.h"
+ 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    int column;
+    int row;
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Chess");
+    sf::RectangleShape tile(sf::Vector2f(100.f, 100.f));
+
 
     while (window.isOpen()) {
+
         sf::Event event;
+
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            window.clear();
+
+            for (row = 0; row <= 7; row++) {
+                for (column = 0; column <= 7; column++) {
+                    if ((row + column) % 2 == 0) {
+                        tile.setFillColor(sf::Color::White);
+
+                    }
+                    else {
+                        tile.setFillColor(sf::Color::Black);
+                    }
+
+                    tile.setPosition(100.f * column, 100.f * row);
+                    window.draw(tile);
+                }
+            }
+
+            window.display();
         }
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
     return 0;
 }
