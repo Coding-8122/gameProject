27ccc83454be023ab5112dfcb7cb24aa51fe32b5
Coding-8122 <hyderahussain@gmail.com>
2026-04-21 11:58:48 +0100

    pieces added to board for both colours

--- main.cpp
+++ main.cpp
@@ -11,20 +11,43 @@ int main() {
     sf::RectangleShape tile(sf::Vector2f(100.f, 100.f));
 
     
-    // loading textures
-    sf::Texture black_texture;
-    sf::Texture white_texture;
+    // loading textures for each piece type and colour from the JohnPablok Chess assets
+    sf::Texture b_pawn_texture, b_king_texture, b_knight_texture, b_bishop_texture, b_rook_texture, b_queen_texture;
+    sf::Texture w_pawn_texture, w_king_texture, w_knight_texture, w_bishop_texture, w_rook_texture, w_queen_texture;
 
-    black_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_pawn_1x.png");     
-    white_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_pawn_1x.png"); 
+    b_pawn_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_pawn_1x.png");
+    b_king_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_king_1x.png");
+    b_knight_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_knight_1x.png");
+    b_bishop_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_bishop_1x.png");
+    b_rook_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_rook_1x.png");
+    b_queen_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/b_queen_1x.png");
+
+    w_pawn_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_pawn_1x.png");
+    w_king_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_king_1x.png");
+    w_knight_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_knight_1x.png");
+    w_bishop_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_bishop_1x.png");
+    w_rook_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_rook_1x.png");
+    w_queen_texture.loadFromFile("JohnPablok Cburnett Chess set/PNGs/With Shadow/1x/w_queen_1x.png");
     
-    Piece black_pawn;
-    Piece white_pawn;
+    
+    Piece b_pawn, b_king, b_knight, b_bishop, b_rook, b_queen;
+    Piece w_pawn, w_king, w_knight, w_bishop, w_rook, w_queen;
 
-    white_pawn.setTexture(white_texture);
-    black_pawn.setTexture(black_texture);
-    black_pawn.setBoardPosition(sf ::Vector2i(0,1));    
-    white_pawn.setBoardPosition(sf ::Vector2i(0,6));
+    b_pawn.setTexture(b_pawn_texture);   
+    b_king.setTexture(b_king_texture);   
+    b_knight.setTexture(b_knight_texture);   
+    b_bishop.setTexture(b_bishop_texture);   
+    b_rook.setTexture(b_rook_texture);   
+    b_queen.setTexture(b_queen_texture);   
+    
+    w_pawn.setTexture(w_pawn_texture);   
+    w_king.setTexture(w_king_texture);   
+    w_knight.setTexture(w_knight_texture);   
+    w_bishop.setTexture(w_bishop_texture);   
+    w_rook.setTexture(w_rook_texture);   
+    w_queen.setTexture(w_queen_texture);   
+    
+
 
 /* Key for board logic:
 1 = Rook 2 = Knight 3 = Bishop 4 = Queen
@@ -69,17 +92,63 @@ Negative numbers = white pieces
                     tile.setPosition(100.f * column, 100.f * row);
                     window.draw(tile);
              
-             // if statement to ensure that pawns are placed on the correct rows of the board relative to 8x8
-            if (row == 1){
-                black_pawn.setBoardPosition(sf ::Vector2i(column,row)); 
-                black_pawn.draw(window);
+             // piece id logic which maps each piece to a number on the previously implemented map and draws on the board accordingly
+            int piece_id = board [row][column];
+
+                if (piece_id == 1){
+                b_rook.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_rook.draw(window);
                 }
-            else if (row == 6){
-                white_pawn.setBoardPosition(sf ::Vector2i(column,row)); 
-                white_pawn.draw(window);
+
+                 if (piece_id == 2){
+                b_knight.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_knight.draw(window);
+                }
+                 if (piece_id == 3){
+                b_bishop.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_bishop.draw(window);
+                }
+                 if (piece_id == 4){
+                b_queen.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_queen.draw(window);
+                }
+                 if (piece_id == 5){
+                b_king.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_king.draw(window);
+                }
+                 if (piece_id == 6){
+                b_pawn.setBoardPosition(sf ::Vector2i(column,row)); 
+                b_pawn.draw(window);
+                }
+               
+                
+             if (piece_id == -1){
+                w_rook.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_rook.draw(window);
+                }
+
+                 if (piece_id == -2){
+                w_knight.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_knight.draw(window);
+                }
+                 if (piece_id == -3){
+                w_bishop.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_bishop.draw(window);
+                }
+                 if (piece_id == -4){
+                w_queen.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_queen.draw(window);
+                }
+                 if (piece_id == -5){
+                w_king.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_king.draw(window);
+                }
+                 if (piece_id == -6){
+                w_pawn.setBoardPosition(sf ::Vector2i(column,row)); 
+                w_pawn.draw(window);
+                }
             }
-            } 
-        } 
+            }  
           window.display();
     }
          
